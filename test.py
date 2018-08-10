d052b793784fe158230d0b7f7200dde12e63332b
a170811 <a170811@gmail.com>
2018-08-10 12:22:12 +0800

    no repeat affix

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ print(word_base('this is a sentence.'))
 ddd = json.load(open('./test.json'))
 print(ddd)
 """
-sentence = "this is a sentence absorbtion avalible pretrain ." 
+sentence = "homeless tired disable mislead unhappily" 
 print( word_affix( sentence , 2 ) )
 
 
